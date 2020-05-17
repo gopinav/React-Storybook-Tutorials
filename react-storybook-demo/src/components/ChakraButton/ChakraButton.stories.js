631107ae5f68aade20c6b4b3b7e21523264b28b7
Vishwas <gopinav5@gmail.com>
2020-05-17 15:33:14 +0530

    Actions addon

--- react-storybook-demo/src/components/ChakraButton/ChakraButton.stories.js
+++ react-storybook-demo/src/components/ChakraButton/ChakraButton.stories.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Button } from '@chakra-ui/core'
+import { action, actions } from '@storybook/addon-actions'
 
 export default {
   title: 'Chakra/Button',
   component: Button
 }
 
-export const Success = () => <Button variantColor='green'>Success</Button>
-export const Danger = () => <Button variantColor='red'>Danger</Button>
+export const Success = () => (
+  <Button variantColor='green' onClick={action('Click handler')}>
+    Success
+  </Button>
+)
+export const Danger = () => (
+  <Button variantColor='red' {...actions('onClick', 'onMouseOver')}>
+    Danger
+  </Button>
+)
