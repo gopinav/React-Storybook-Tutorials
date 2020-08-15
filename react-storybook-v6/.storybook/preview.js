2e06b7e4629bfd289d60f1f4ce486bd39623ad6d
Vishwas <vishwas.g5@gmail.com>
2020-08-15 14:59:25 +0530

    Renaming and sorting stories

--- react-storybook-v6/.storybook/preview.js
+++ react-storybook-v6/.storybook/preview.js
@@ -1,4 +1,8 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  }
 }
--- react-storybook-v6/src/components/Input/Input.stories.js
+++ react-storybook-v6/src/components/Input/Input.stories.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Input from './Input'
 
 export default {
-  title: 'Form/Input',
+  title: 'Form/AInput',
   component: Input
 }
 
