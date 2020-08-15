0c3f033a9d844a7d07abc26d4d0400d761c5e552
Vishwas <vishwas.g5@gmail.com>
2020-08-15 14:22:56 +0530

    Renaming stories

--- react-storybook-v6/src/components/Input/Input.stories.js
+++ react-storybook-v6/src/components/Input/Input.stories.js
@@ -9,3 +9,5 @@ export default {
 export const Small = () => <Input size='small' placeholder='Small size' />
 export const Medium = () => <Input size='medium' placeholder='Medium size' />
 export const Large = () => <Input size='large' placeholder='Large size' />
+
+Small.storyName = 'Small Input'
