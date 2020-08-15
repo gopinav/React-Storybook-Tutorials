5fa2bcb5ce592c5656d1209a30c75fe02a280695
Vishwas <vishwas.g5@gmail.com>
2020-08-15 22:08:02 +0530

    Story within story

---
+++ react-storybook-v6/src/components/Subscription/Subscription.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Primary } from '../Button/Button.stories'
+import { Large } from '../Input/Input.stories'
+
+export default {
+  title: 'Form/Subscription',
+}
+
+export const PrimarySubscription = () => (
+  <>
+    <Large />
+    <Primary />
+  </>
+)
