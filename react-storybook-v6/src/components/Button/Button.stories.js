3fe34e11da4462f08f73ffac819a9c4dd70f99f3
Vishwas <vishwas.g5@gmail.com>
2020-08-17 18:30:37 +0530

    args mechanism

--- react-storybook-v6/src/components/Button/Button.stories.js
+++ react-storybook-v6/src/components/Button/Button.stories.js
@@ -3,10 +3,24 @@ import Button from './Button'
 
 export default {
   title: 'Form/Button',
-  component: Button
+  component: Button,
 }
 
 export const Primary = () => <Button variant='primary'>Primary</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
+const Template = args => <Button {...args} />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+  variant: 'primary',
+  children: 'Primary Args',
+}
+
+export const SecondaryA = Template.bind({})
+SecondaryA.args = {
+  variant: 'secondary',
+  children: 'Secondary Args',
+}
